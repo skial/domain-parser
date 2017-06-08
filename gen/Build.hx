9b18265e288fcf0bd3a418c2296dc434f2ef7288
Skial Bainn <skial.bainn@gmail.com>
2017-06-08 11:03:45 +0100

    Auto generate uhx/types/DomainSpec.hx from gen/Build.hx Rewrite the search portion of uhx/types/Domain.hx, as it was wrong before hand. So far only neko can run all generated tests, node throws `throw exec` related to utest.

--- gen/Build.hx
+++ gen/Build.hx
@@ -23,6 +23,9 @@ typedef Mapped = AssociativeArray<Map<String, Mapped>>;
 // Based on https://github.com/peerigon/parse-domain/blob/master/lib/build/buildRegex.js
 class Build {
 
+    private static var clsDomain:String = '${Sys.getCwd()}/src/uhx/types/Domain.hx';
+    private static var clsDomainSpec:String = '${Sys.getCwd()}/tests/uhx/types/DomainSpec.hx';
+
     public static function generate() {
         var hash = new Map<String, Node>();
         var index = new Map<String, GraphNode<Node>>();
@@ -41,11 +44,73 @@ class Build {
         icannTlds = ~/\./g.replace(icannTlds, ".");
         icannTlds = icannTlds.substring(1, icannTlds.length-1);
         var icannItems = icannTlds.split('|');
+        var tests = [];
         
         for (item in icannItems) {
             var parts = item.split('.');
             var node:GraphNode<Node> = null;
-            
+
+            if (!item.startsWith('!') && !item.startsWith('::') && !item.startsWith('*')) {
+                // TODO handle idn mapping to cctld.
+                var methodName = ~/[-\\.\\*]+/gi.replace('test_' + item, '_');
+                var domain = 'www.example.' + item;
+
+                var tmp = '';
+                for (i in 0...methodName.uLength()) if (methodName.uCharCodeAt(i) > 128) {
+                    tmp += methodName.uCharCodeAt(i).hex();
+                } else {
+                    tmp += methodName.uCharAt(i);
+                }
+
+                tests.push( (macro class Tmp {
+
+                    public function $tmp() {
+                        var values = $v{domain}.parse();
+
+                        switch values {
+                            case haxe.ds.Option.Some(parts):
+                                utest.Assert.equals( 3, parts.length );
+                                switch parts[0] {
+                                    case uhx.types.domains.DomainParts.Subdomain(s):
+                                        utest.Assert.equals( 1, s.length );
+                                        utest.Assert.equals('www', s[0]);
+
+                                    case _:
+                                        utest.Assert.fail('Subdomain should not be empty.');
+
+                                }
+                                //utest.Assert.isTrue( parts[0].match(uhx.types.domains.DomainParts.Subdomain(['www'])), 'Should be Subdomain containing `[www]`.' );
+                                switch parts[1] {
+                                    case uhx.types.domains.DomainParts.Domain(s):
+                                        utest.Assert.equals('example', s);
+
+                                    case _:
+                                        utest.Assert.fail('Domain should not be empty.');
+
+                                }
+                                //utest.Assert.isTrue( parts[1].match(uhx.types.domains.DomainParts.Domain('example')), 'Should be Domain name `example`.' );
+                                switch parts[2] {
+                                    case uhx.types.domains.DomainParts.Tld(s):
+                                        utest.Assert.equals( $v{parts.length}, s.length );
+                                        utest.Assert.equals('' + $a{parts.map(s->macro$v{s})}, '' + s);
+
+                                    case _:
+                                        utest.Assert.fail('Tld should not be empty.');
+
+                                }
+                                //utest.Assert.isTrue( parts[2].match(uhx.types.domains.DomainParts.Tld([$a{parts.map(s->macro$v{s})}])), $v{'Should be Tld containing `[' + parts.join(', ') + ']`.'} );
+
+                            case haxe.ds.Option.None:
+                                utest.Assert.fail($v{'$domain should not have parsed into an empty result.'});
+
+                        }
+
+                    }
+
+                }).fields[0] );
+
+            }
+
             parts.reverse();
 
             for (i in 0...parts.length) {
@@ -126,13 +191,36 @@ class Build {
         var graphExpr = convertGraph(graph, index);
         //'${Sys.getCwd()}/info.txt'.saveContent( new haxe.macro.Printer().printExpr(graphExpr).replace('de.polygonal.ds.Graph', 'Graph').replace('uhx.types.domains.Node', 'Node') );
 
-        var td = macro class Domain {
+        var tdDomainSpec = macro class DomainSpec {
+            public function new() {
+
+            }
+        }
+
+        tdDomainSpec.fields = tdDomainSpec.fields.concat( tests );
+
+        var tdDomain = macro class Domain {
             public static function exists(tld:String):Bool {
                 return map.exists(tld);
             }
 
             public static function get(tld:String):Null<de.polygonal.ds.GraphNode<uhx.types.domains.Node>> {
                 return map.get(tld);
+            }
+
+            private static function search(node:de.polygonal.ds.GraphNode<uhx.types.domains.Node>, val:String):Null<de.polygonal.ds.GraphNode<uhx.types.domains.Node>> {
+                var result = null;
+				
+				for (linked in node) {
+					if (linked.val == val) {
+						result = graph.findNode(linked);
+						break;
+
+					}
+
+				}
+				
+				return result;
             }
 
             public static function parse(domain:String, ?tlds:Array<String->Bool>, ?slds:Array<String->Bool>):haxe.ds.Option<Array<uhx.types.domains.DomainParts>> {
@@ -171,9 +259,9 @@ class Build {
 
                     results.tlds.push( segment );
                     idx--;
-                    depth--;
+                    //depth--;
 
-                    while (depth > 0) {
+                    /*while (depth > 0) {
                         if (graphNode.numArcs > 0) for (linked in graphNode.iterator()) {
                             if (linked.val == segments[idx]) {
                                 results.tlds.push( linked.val );
@@ -187,6 +275,19 @@ class Build {
 
                         } else {
                             break;
+
+                        }
+
+                    }*/
+
+                    
+                    while (graphNode != null && graphNode.numArcs > 0) {
+                        graphNode = search(graphNode, segments[idx--]);
+                        if (graphNode != null) {
+                            results.tlds.push( graphNode.val.val ); 
+
+                        } else {
+                            idx++;
 
                         }
 
@@ -231,15 +332,35 @@ class Build {
             
         }
         
-        td.pack = ['uhx', 'types'];
+        tdDomain.pack = ['uhx', 'types'];
         
-        var output = new haxe.macro.Printer().printTypeDefinition(td, true);
+        var output = new haxe.macro.Printer().printTypeDefinition(tdDomain, true);
         var parts = output.split(' uhx.types;');
 
         parts[0] += ' uhx.types;\r\nimport haxe.ds.Option;\r\nimport uhx.types.domains.DomainParts;\r\n// Autogenerated by the build macro `gen.Build`.';
         parts[1] = parts[1].replace('haxe.ds.Option.', '').replace('uhx.types.domains.DomainParts.', '');
 
-        '${Sys.getCwd()}/src/uhx/types/Domain.hx'.saveContent(parts.join(''));
+        var root = index.get('jp');
+        trace( root.val, root.numArcs );
+        for (linked in root) {
+            if (linked.val == 'kanagawa') {
+                var g = graph.findNode(linked);
+                trace(g);
+                for (linked in g) {
+                    if (linked.val == 'aikawa') {
+                        trace(graph.findNode(linked));
+                        break;
+
+                    }
+
+                }
+                break;
+
+            }
+
+        }
+        clsDomain.saveContent(parts.join(''));
+        clsDomainSpec.saveContent('package uhx.types;\r\nimport uhx.types.Domain;\r\nusing uhx.types.Domain;\r\n@:keep\r\n' + new haxe.macro.Printer().printTypeDefinition(tdDomainSpec, true));
     }
 
     private static function convertGraph(graph:Graph<Node>, root:Map<String, GraphNode<Node>>):Expr {
@@ -257,11 +378,13 @@ class Build {
         } );
 
         graph.iter( function(node) {
-            if (node.type) {
-                if (node.segments > 1) {
+            var g = graph.findNode(node);
+            //if (node.type) {
+                if (g.numArcs > 0) {
                     var targets = [];
                     
-                    for (target in root.get(node.val).iterator()) {
+                    //for (target in root.get(node.val).iterator()) {
+                    for (target in graph.findNode(node).iterator()) {                        
                         targets.push( macro $v{positionMap.get(target.val)} );
 
                     }
@@ -273,7 +396,7 @@ class Build {
                     
                 }
 
-            }
+            //}
 
         } );
         
